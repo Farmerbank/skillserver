e4a3296ff1f95efdfeeb4b94cb098b4070e8e334
tvs <teije-vs@hotmail.com>
2017-05-18 23:11:22 +0200

    Added bwfclient impl

--- main.go
+++ main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+
 	alexa "github.com/mikeflynn/go-alexa/skillserver"
+	"github.com/naipath/bwfclient"
 )
 
-var applications = map[string]interface{}{
-	"/echo/helloworld": alexa.EchoApplication{
-		AppID:    "amzn1.ask.skill.8c0cabc9-c18b-4d53-ac7f-61e2d6f367a2",
-		OnLaunch: launchIntentHandler,
-		OnIntent: echoIntentHandler,
-	},
-}
+var (
+	bwfClient    = bwfclient.New()
+	applications = map[string]interface{}{
+		"/echo/helloworld": alexa.EchoApplication{
+			AppID:    "amzn1.ask.skill.8c0cabc9-c18b-4d53-ac7f-61e2d6f367a2",
+			OnLaunch: launchIntentHandler,
+			OnIntent: echoIntentHandler,
+		},
+	}
+)
 
 func main() {
-	alexa.Run(applications, "3000")
+
+	resp, err := bwfClient.Request(40000, 0)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Print(resp.MaxTeLenenObvInkomen.Tienjaarsrente.KoopsomBedr)
+
+	// alexa.Run(applications, "3000")
 }
 
 func launchIntentHandler(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
@@ -21,5 +36,13 @@ func launchIntentHandler(echoReq *alexa.EchoRequest, echoResp *alexa.EchoRespons
 }
 
 func echoIntentHandler(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
-	echoResp.OutputSpeech("Hello world from my new Echo test app!")
+	echoResp.OutputSpeech("Hello world from my new Echo test app! " + strconv.Itoa(retrieveKoopsomBedr()))
+}
+
+func retrieveKoopsomBedr() int {
+	resp, err := bwfClient.Request(40000, 0)
+	if err != nil {
+		fmt.Print(err)
+	}
+	return resp.MaxTeLenenObvInkomen.Tienjaarsrente.KoopsomBedr
 }
