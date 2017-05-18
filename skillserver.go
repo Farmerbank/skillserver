b652df77ef6a75beaf1962c39da9fca2fdde89b9
tvs <teije-vs@hotmail.com>
2017-05-18 21:16:23 +0200

    Added launch intent

--- skillserver.go
+++ skillserver.go
@@ -7,8 +7,8 @@ import (
 var Applications = map[string]interface{}{
 	"/echo/helloworld": alexa.EchoApplication{ // Route
 		AppID:    "amzn1.ask.skill.8c0cabc9-c18b-4d53-ac7f-61e2d6f367a2", // Echo App ID from Amazon Dashboard
-		OnIntent: EchoIntentHandler,
-		OnLaunch: EchoIntentHandler,
+		OnLaunch: launchIntentHandler,
+		OnIntent: echoIntentHandler,
 	},
 }
 
@@ -16,6 +16,10 @@ func main() {
 	alexa.Run(Applications, "3000")
 }
 
-func EchoIntentHandler(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
+func launchIntentHandler(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
+	echoResp.OutputSpeech("You just launched the Farmerbank app!")
+}
+
+func echoIntentHandler(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
 	echoResp.OutputSpeech("Hello world from my new Echo test app!").Card("Hello World", "This is a test card.")
 }
