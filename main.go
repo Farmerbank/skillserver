09e6484e1e64d6df1c8153045314807fc1e69ed7
Wian Vos <wian.vos@cinqict.nl>
2017-05-19 03:13:24 +0200

    reverted to teije style

--- main.go
+++ main.go
@@ -1,20 +1,52 @@
 package main
 
 import (
-	"github.com/farmerbank/skillserver/skills/helloworld"
+	"fmt"
+	"strconv"
+
 	alexa "github.com/mikeflynn/go-alexa/skillserver"
+	"github.com/naipath/bwfclient"
 )
 
-func getApps() map[string]interface{} {
-	a := make(map[string]interface{})
-
-	a["/echo/helloworld"] = helloworld.HWGetApp()
-
-	return a
-}
+var (
+	bwfClient    = bwfclient.New()
+	applications = map[string]interface{}{
+		"/echo/helloworld": alexa.EchoApplication{
+			AppID:    "amzn1.ask.skill.a1c73b55-4e76-45f2-8478-bc79b77cc537",
+			OnLaunch: launchIntentHandler,
+			OnIntent: echoIntentHandler,
+		},
+	}
+)
 
 func main() {
+	alexa.Run(applications, "3000")
+}
 
-	alexa.Run(getApps(), "3000")
+func launchIntentHandler(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
+	echoResp.OutputSpeech("You just launched the Farmerbank app!    Moooh").EndSession(false)
+}
 
+func echoIntentHandler(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
+	switch echoReq.GetIntentName() {
+	case "GetBalance":
+		echoResp.OutputSpeech("You can loan " + strconv.Itoa(retrieveKoopsomBedr())).EndSession(false)
+
+	case "ElevatorPitch":
+		echoResp.OutputSpeech("Starting the elevatorpitch. With this application it is possible to interact with your bank account in various ways using a natural dialogue.").EndSession(false)
+
+	default:
+		echoResp.OutputSpeech("Unrecognized command").EndSession(false)
+	}
+}
+
+func retrieveKoopsomBedr() int {
+	resp, err := bwfClient.Request(bwfclient.BwfRequest{
+		AanvragerBrutoJaarinkomenBedr: 40000,
+		PartnerBrutoJaarinkomenBedr:   0,
+	})
+	if err != nil {
+		fmt.Print(err)
+	}
+	return resp.MaxTeLenenObvInkomen.Tienjaarsrente.KoopsomBedr
 }
