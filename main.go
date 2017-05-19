77d339415c873a9c56974bf409c6e31654608c72
Wian Vos <wian.vos@cinqict.nl>
2017-05-19 02:51:04 +0200

    decoupled main from skills

--- main.go
+++ main.go
@@ -1,43 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
-
+	"github.com/farmerbank/skillserver/skills/helloworld"
 	alexa "github.com/mikeflynn/go-alexa/skillserver"
-	"github.com/naipath/bwfclient"
 )
 
-var (
-	bwfClient    = bwfclient.New()
-	applications = map[string]interface{}{
-		"/echo/helloworld": alexa.EchoApplication{
-			AppID:    "amzn1.ask.skill.8c0cabc9-c18b-4d53-ac7f-61e2d6f367a2",
-			OnLaunch: launchIntentHandler,
-			OnIntent: echoIntentHandler,
-		},
-	}
-)
+func getApps() map[string]interface{} {
+	a := make(map[string]interface{})
+
+	a["/echo/helloworld"] = helloworld.HWGetApp()
+
+	return a
+}
 
 func main() {
-	alexa.Run(applications, "3000")
-}
 
-func launchIntentHandler(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
-	echoResp.OutputSpeech("You just launched the Farmerbank app!")
-}
+	alexa.Run(getApps(), "3000")
 
-func echoIntentHandler(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
-	echoResp.OutputSpeech("Hello world from my new Echo test app! " + strconv.Itoa(retrieveKoopsomBedr()))
-}
-
-func retrieveKoopsomBedr() int {
-	resp, err := bwfClient.Request(bwfclient.BwfRequest{
-		AanvragerBrutoJaarinkomenBedr: 40000,
-		PartnerBrutoJaarinkomenBedr:   0,
-	})
-	if err != nil {
-		fmt.Print(err)
-	}
-	return resp.MaxTeLenenObvInkomen.Tienjaarsrente.KoopsomBedr
 }
